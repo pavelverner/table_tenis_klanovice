3f2edc40561e13a1e1deafd060e5af824bee8d36
pavelverner <pavel.verner@post.cz>
2026-03-22 21:09:10 +0100

    data: aktualizace s časem

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ const CLUB_DATA = {
   "stisUrl": "https://stis.ping-pong.cz/oddil-420109007/svaz-420101/rocnik-2025",
   "rocnik": 2025,
   "season": "2025/26",
-  "lastUpdate": "22. 3. 2026",
+  "lastUpdate": "22.03.26 21:06",
   "teams": [
     {
       "id": 63401,
